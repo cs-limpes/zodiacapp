d6d81e1c1f16478009121283ecb6139e0af2f516
Eddie <educatedsavage@educatedsavage.com>
2024-02-18 01:59:46 -0800

    setting up clerk for authentication and converting to a vite project as a result

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react"
 
 function App() {
+  
   return (
+
     <div className="App">
+      <SignedOut>
+        <SignInButton />
+        <p>This content is public. Only signed out users can see the SignInButton above this text.</p>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton signOutCallback={() => redirect('/')} />
+        <p>This content is private. Only signed in users can see the SignOutButton above this text.</p>
+      </SignedIn>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+// Import your clerk publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+ 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
 
